95fb81e8971eae9a232cd4371a5b9bd49aea3e9f
haseebsultan <38536290+haseebsultan@users.noreply.github.com>
2020-12-14 15:10:21 +0500

    Update buildbar.sh

--- buildbar.sh
+++ buildbar.sh
@@ -5,7 +5,7 @@
 echo 'building bar file'
 cd /opt/ace-11.0.0.9/tools
 
- . /opt/ace-11.0.0.9/server/bin/mqsiprofile && mqsicreatebar -data $WORKSPACE -b $WORKSPACE/GeneratedBarFiles/test.bar  -a Calculator -compileOnly -v createbartrace.txt -cleanBuild 
+ . /opt/ace-11.0.0.9/server/bin/mqsiprofile && mqsicreatebar -data $WORKSPACE -b $WORKSPACE/GeneratedBarFiles/${BUILD_ID}.bar  -a Calculator -compileOnly -v createbartrace.txt -cleanBuild 
 
 
                  
