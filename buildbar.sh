9d475e92d618e293a5f93291d370fcf9e14617e8
haseebsultan <38536290+haseebsultan@users.noreply.github.com>
2020-12-14 14:39:52 +0500

    Create buildbar.sh

---
+++ buildbar.sh
@@ -0,0 +1,10 @@
+
+#worksp=$1
+#barname=$2
+
+echo 'building bar file'
+
+./opt/ace-11.0.0.9/tools/mqsicreatebar -data $WORKSPACE -b $WORKSPACE/GeneratedBarFiles/test.bar  -a Calculator -compileOnly -v createbartrace.txt -cleanBuild' 
+
+
+                 
