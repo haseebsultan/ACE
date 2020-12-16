c3b5739848463afae39f4cb7970b3341a0ee8b92
haseebsultan <38536290+haseebsultan@users.noreply.github.com>
2020-12-16 18:52:03 +0500

    Update buildbar.sh

--- buildbar.sh
+++ buildbar.sh
@@ -8,7 +8,7 @@ cd /opt/ace-11.0.0.9/tools
  . /opt/ace-11.0.0.9/server/bin/mqsiprofile && mqsicreatebar -data $WORKSPACE -b $WORKSPACE/GeneratedBarFiles/${BUILD_ID}.bar  -a Calculator -compileOnly -v createbartrace.txt -cleanBuild 
 
 cd /$WORKSPACE/GeneratedBarFiles
-git init
+
 git add .
 git commit -m "Adding file"
 git remote add origin "https://github.com/haseebsultan/ACE.git"
